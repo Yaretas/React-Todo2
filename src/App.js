11bd645d785accd9af55116efa74fb0e421532d1
Yara Castro <yaracastro24@gmail.com>
2019-11-05 14:08:59 -0600

    added some style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TodoList from './components/TodoComponents/TodoList';
 import TodoForm from './components/TodoComponents/TodoForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/TodoComponents/Todo.css';
 
 
 class App extends React.Component {
@@ -67,7 +68,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="App">
         <h2>Today Todos</h2>
         <TodoList todos={this.state.todos} toggleTodo={this.toggleTodo} />
         <TodoForm
